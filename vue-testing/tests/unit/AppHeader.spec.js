cf212cb25c2ff5fc352ea65089cfc302818dd930
alvinzero1 <115516782+alvinzero1@users.noreply.github.com>
2023-04-13 17:51:47 +0800

    Add files via upload

---
+++ vue-testing/tests/unit/AppHeader.spec.js
@@ -0,0 +1,16 @@
+import { mount } from "@vue/test-utils";
+import AppHeader from "@/components/AppHeader";
+
+describe("AppHeader", () => {
+    test("if logged in is false, do not show logout button", () => {
+        const wrapper = mount(AppHeader);
+        expect(wrapper.find("button").isVisible()).toBe(false);
+    });
+
+    test("if logged in, show logout button", () => {
+        const wrapper = mount(AppHeader);
+        wrapper.setData({ loggedIn: true });
+        expect(wrapper.find("button").isVisible()).toBe(true);
+        /** not working ! */
+    });
+});
