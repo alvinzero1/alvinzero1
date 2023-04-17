191e7bc6975588bc099254b3391f2263def76f3b
alvinzero1 <115516782+alvinzero1@users.noreply.github.com>
2023-04-17 16:07:09 +0800

    Add files via upload

---
+++ vueCreate_jest/src/components/FruitList.vue
@@ -0,0 +1,29 @@
+<!-- FruitList.vue -->
+
+<template>
+    <div>
+      <ol>
+        <li v-for="fruit in fruits" :key="fruit">
+          {{ fruit }}
+        </li>
+      </ol>
+      <p>
+        Number of fruits: {{ numFruits }}
+      </p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        fruits: ["apple", "banana", "orange"],
+      };
+    },
+    computed: {
+      numFruits() {
+        return this.fruits.length;
+      },
+    },
+  };
+  </script>
