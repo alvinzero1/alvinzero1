44f2fe7ce980e1705a6df9946bb6a65e3a3474bf
alvinzero1 <115516782+alvinzero1@users.noreply.github.com>
2023-04-19 16:07:24 +0800

    Add files via upload

---
+++ SpringJsonVue/src/frontend/src/components/DisplayJson.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, watchEffect } from 'vue'
+
+const API_URL = `/api/messages/json`
+const idText = ref([])
+
+function getJson() {
+  console.log(">>>>>")
+  watchEffect(async () => {
+    const url = `${API_URL}`
+    idText.value = await (await fetch(url)).json()
+  })
+}
+</script>
+<template>
+  <div>
+    <button @click="getJson">get JSON</button>
+
+    <div>
+      ID: {{ idText.id }}
+    </div>
+    <div>
+      Name: {{ idText.name }}
+    </div>
+    <div>
+      DOB: {{ idText.birthDate }}
+    </div>
+    <div>
+      Email: {{ idText.emails }}
+    </div>
+  </div>
+</template>
